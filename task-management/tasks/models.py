1ac7e1c051608eefbd76b7979fd90b3148ca2f5c
Md Shagor (Musa daNyal Shagor) <mdshagor.three.six.nine@gmail.com>
2025-02-19 13:20:48 +0600

    module 11.3 bug fix, also the datbase model fixed

---
+++ task-management/tasks/models.py
@@ -0,0 +1,96 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save, pre_save, m2m_changed, post_delete
+from django.dispatch import receiver
+from django.core.mail import send_mail
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('COMPLETED', 'Completed')
+    ]
+    project = models.ForeignKey(
+        "Project",
+        on_delete=models.CASCADE,
+        default=1
+    )
+    assigned_to = models.ManyToManyField(Employee, related_name='tasks')
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    due_date = models.DateField()
+    status = models.CharField(
+        max_length=15, choices=STATUS_CHOICES, default="PENDING")
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # details
+
+    def __str__(self):
+        return self.title
+
+
+class TaskDetail(models.Model):
+    HIGH = 'H'
+    MEDIUM = 'M'
+    LOW = 'L'
+    PRIORITY_OPTIONS = (
+        (HIGH, 'High'),
+        (MEDIUM, 'Medium'),
+        (LOW, 'Low')
+    )
+    task = models.OneToOneField(
+        Task,
+        on_delete=models.DO_NOTHING,
+        related_name='details',
+    )
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
+    priority = models.CharField(
+        max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Details form Task {self.task.title}"
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    start_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+# Signals
+
+
+@receiver(m2m_changed, sender=Task.assigned_to.through)
+def notify_employees_on_task_creation(sender, instance, action, **kwargs):
+    if action == 'post_add':
+        print(instance, instance.assigned_to.all())
+        assigned_email = [emp.email for emp in instance.assigned_to.all()]
+        print("Cheacking!", assigned_email)
+        send_mail(
+            "New Task Assigned",
+            f"You have been assigned to the task: {instance.title}",
+            "mdshagor.three.six.nine@gmail.com",
+            assigned_email,
+            fail_silently= False,
+    )
+    
+@receiver(post_delete, sender=Task)
+def delete_associate_details(sender, instance, **kwargs):
+    if instance.details:
+        print(instance)
+        instance.details.delete()
+        
+        
+        print("Deleted Succelfully! ")
---
+++ task-management/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from users.views import sign_up, sign_in, sign_out
+
+
+#adding path from users
+urlpatterns = [
+    path('sign-up/', sign_up, name='sign-up'),
+    path('sign-in/', sign_in, name='sign-in'),
+    path('sign-out/', sign_out, name='logout')
+]
