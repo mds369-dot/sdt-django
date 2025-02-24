4b90dccc6158cde0aefe801570d743b6152b3951
Md Shagor (Musa daNyal Shagor) <mdshagor.three.six.nine@gmail.com>
2025-02-24 09:05:01 +0600

    user base role and user system add

--- task-management/tasks/models.py
+++ task-management/tasks/models.py
@@ -1,8 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save, pre_save, m2m_changed, post_delete
-from django.dispatch import receiver
-from django.core.mail import send_mail
+
 
 class Employee(models.Model):
     name = models.CharField(max_length=100)
@@ -52,7 +49,7 @@ class TaskDetail(models.Model):
         on_delete=models.DO_NOTHING,
         related_name='details',
     )
-    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
+    # assigned_to = models.CharField(max_length=100)
     priority = models.CharField(
         max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
     notes = models.TextField(blank=True, null=True)
@@ -68,29 +65,3 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
-
-# Signals
-
-
-@receiver(m2m_changed, sender=Task.assigned_to.through)
-def notify_employees_on_task_creation(sender, instance, action, **kwargs):
-    if action == 'post_add':
-        print(instance, instance.assigned_to.all())
-        assigned_email = [emp.email for emp in instance.assigned_to.all()]
-        print("Cheacking!", assigned_email)
-        send_mail(
-            "New Task Assigned",
-            f"You have been assigned to the task: {instance.title}",
-            "mdshagor.three.six.nine@gmail.com",
-            assigned_email,
-            fail_silently= False,
-    )
-    
-@receiver(post_delete, sender=Task)
-def delete_associate_details(sender, instance, **kwargs):
-    if instance.details:
-        print(instance)
-        instance.details.delete()
-        
-        
-        print("Deleted Succelfully! ")
